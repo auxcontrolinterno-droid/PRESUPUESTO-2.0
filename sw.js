00e76f4a8cc2f8758e404a65764f435a6bb4f4a4
auxcontrolinterno-droid <auxcontrolinterno@altamira.edu.co>
2026-04-28 23:31:29 -0500

    Update service worker cache and fetch logic

--- sw.js
+++ sw.js
@@ -1,42 +1,43 @@
-const CACHE_NAME = "PRESUPUESTO-2.2.1";
+const CACHE_NAME = "PRESUPUESTO-PRO-3.0";
 
-const FILES_TO_CACHE = [
+const FILES = [
+  "./",
   "./index.html",
   "./manifest.json",
-  "./sw.js"
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// INSTALAR
 self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
   );
 });
 
+// ACTIVAR
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
-      );
-    }).then(() => self.clients.claim())
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
+// FETCH (estrategia estable tipo app real)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request)
-      .then((response) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return res;
       })
       .catch(() => caches.match(event.request))
   );
